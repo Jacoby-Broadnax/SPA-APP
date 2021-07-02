bb0cb8b256e743465bdfdd0939df2c230757205f
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-01 21:01:00 -0500

    Loading Fixes

--- actionSheet.js
+++ actionSheet.js
@@ -1,9 +1,11 @@
 function dynSheet(name, url){
-document.getElementById('dyn-sheet').style.display = 'block';
 document.getElementById('sheetTitle').innerHTML = name;
+document.getElementById('sheetFrame').src= '';  
 document.getElementById('sheetFrame').src= url;
+document.getElementById('item-loader').style.display = 'block';
 }
 
 function dynClose(){
-document.getElementById('dyn-sheet').style.display = 'none';   
+document.getElementById('dyn-sheet').style.display = 'none';
+  
 }
