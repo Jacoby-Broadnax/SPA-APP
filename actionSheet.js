aa1f43071fdee3a93a724c9ab3fdcf8293ddba10
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-01 20:46:54 -0500

    Product Link Test

--- actionSheet.js
+++ actionSheet.js
@@ -1,3 +1,9 @@
 function dynSheet(name, url){
 document.getElementById('dyn-sheet').style.display = 'block';
+document.getElementById('sheetTitle').innerHTML = name;
+document.getElementById('sheetFrame').src= url;
+}
+
+function dynClose(){
+document.getElementById('dyn-sheet').style.display = 'none';   
 }
