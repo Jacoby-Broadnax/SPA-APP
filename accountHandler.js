3c07971832f4e533243359f4aac5760dfddcf4c3
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-07 21:38:23 -0500

    Macot PackageUpdate

--- accountHandler.js
+++ accountHandler.js
@@ -6,6 +6,7 @@ if ("userState" in localStorage){
     document.getElementById('pIcon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
     document.getElementById('fy1img').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
     document.getElementById('fy1icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1title').innerHTML= localStorage.getItem('dataschool');
 }else{
     document.getElementById('initial').style.display='block';
 }
