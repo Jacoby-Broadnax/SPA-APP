876dad78e3ed2a7982066f9963fe6190aa8da2c3
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-07 19:15:20 -0500

    Sign Up Flow Fixes

--- accountHandler.js
+++ accountHandler.js
@@ -1,3 +1,9 @@
 function accountStore(title, desc){
 localStorage.setItem('data' + title, desc);
 }
+
+if ("userState" in localStorage){
+   
+}else{
+    document.getElementById('initial').style.display='block';
+}
