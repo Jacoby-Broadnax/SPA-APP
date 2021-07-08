14c0c603be412f924bcd9a4da0fe81bec4cb5eed
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-08 11:53:01 -0500

    Profile Page Test

--- accountHandler.js
+++ accountHandler.js
@@ -7,8 +7,26 @@ if ("userState" in localStorage){
     document.getElementById('fy1img').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
     document.getElementById('fy1icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
     document.getElementById('fy1title').innerHTML= localStorage.getItem('dataschool');
-    document.getElementById('fy2img').src= 'announcements/' + localStorage.getItem('dataschool') + '.jpg'
+    document.getElementById('fy2img').src= 'announcements/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy2title').innerText = 'For ' + localStorage.getItem('datafname');
+    document.getElementById('overlay-icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('PfirstName').innerHTML= localStorage.getItem('datafname');
+    document.getElementById('PlastName').innerHTML = localStorage.getItem('datalname');
+    document.getElementById('Pmail').innerHTML = localStorage.getItem('dataemail')
 }else{
     document.getElementById('initial').style.display='block';
+}
+
+function userState(){
+    document.getElementById('pIcon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1img').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1title').innerHTML= localStorage.getItem('dataschool');
+    document.getElementById('fy2img').src= 'announcements/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy2title').innerText = 'For ' + localStorage.getItem('datafname');
+    document.getElementById('overlay-icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('PfirstName').innerHTML= localStorage.getItem('datafname');
+    document.getElementById('PlastName').innerHTML = localStorage.getItem('datalname');
+    document.getElementById('Pmail').innerHTML = localStorage.getItem('dataemail')
 }
 
