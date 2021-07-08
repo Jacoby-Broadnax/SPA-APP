275c7f21f767d2a768d799fc844da03086472e86
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-07 21:15:40 -0500

    For You Button

--- accountHandler.js
+++ accountHandler.js
@@ -3,7 +3,9 @@ localStorage.setItem('data' + title, desc);
 }
 
 if ("userState" in localStorage){
-    document.getElementById('pIcon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg'
+    document.getElementById('pIcon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1img').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
+    document.getElementById('fy1icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
 }else{
     document.getElementById('initial').style.display='block';
 }
