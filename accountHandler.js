50be397f0effb5644678a4b67b7103a8afcf0e5f
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-07 20:47:30 -0500

    Added Profile Icon

--- accountHandler.js
+++ accountHandler.js
@@ -3,7 +3,8 @@ localStorage.setItem('data' + title, desc);
 }
 
 if ("userState" in localStorage){
-   
+    document.getElementById('pIcon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg'
 }else{
     document.getElementById('initial').style.display='block';
 }
+
