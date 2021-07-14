b7b5ed94da00e469353372c951ebbecb6e48d1b8
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-13 20:21:13 -0500

    Update actionSheet.js

--- actionSheet.js
+++ actionSheet.js
@@ -3,10 +3,12 @@ document.getElementById('sheetTitle').innerHTML = name;
 document.getElementById('sheetFrame').src= "";
 document.getElementById('sheetFrame').src= url;
 document.getElementById('item-loader').style.display = 'block';
-
+document.getElementById('cartFrame').src='https://spa.ms/cart/';
 }
 
 function dynClose(){
 document.getElementById('dyn-sheet').style.display = 'none';
 document.getElementById('sheetFrame').src= '';  
+
+
 }
