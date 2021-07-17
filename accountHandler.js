640d30827b43417647b1666ecad7a8840c362a08
Jacoby Broadnax <32787662+JacobyBX@users.noreply.github.com>
2021-07-17 13:32:52 -0500

    Added Load Bundler

--- accountHandler.js
+++ accountHandler.js
@@ -12,9 +12,14 @@ if ("userState" in localStorage){
     document.getElementById('overlay-icon').src = 'logo/' + localStorage.getItem('dataschool') + '.jpg';
     document.getElementById('PfirstName').innerHTML= localStorage.getItem('datafname');
     document.getElementById('PlastName').innerHTML = localStorage.getItem('datalname');
-    document.getElementById('Pmail').innerHTML = localStorage.getItem('dataemail')
+    document.getElementById('Pmail').innerHTML = localStorage.getItem('dataemail');
 }else{
     document.getElementById('initial').style.display='block';
+    document.getElementById('loadGrad').src = "https://spa.ms/i11/";
+    document.getElementById('loadProduct').src = "https://spa.ms/senior-products/";
+    document.getElementById('loadGear').src = "https://spa.ms/sweatshirt/";
+    document.getElementById('loadRing').src = "https://spa.ms/i11/";
+    document.getElementById('loadWRing').src = "https://spa.ms/i21/";
 }
 
 function userState(){
